19db0310b32eb9af3fc1c60a15d4ccc6d60c9fcc
Alvin Cherk <alvin@cherk.org>
2022-07-16 18:40:27 +1000

    Initial commit

---
+++ src/type.py
@@ -0,0 +1,18 @@
+# 'a' is of type int
+a = 1
+
+# # 'b' is of type float
+b = 1.0
+
+# 'c' is of type str
+c = 'a'
+
+# 'd' is of type str
+d = 'mtrn4110'
+
+# 'e' is of type bool
+e = True
+
+print(a)
+
+print(type(a)) # prints int
